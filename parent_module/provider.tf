762a50c6c57405a050cc64c89bf5a39c8e79f0f6
Shekhar281 <sg.shekhargupta28@gmail.com>
2025-12-24 19:27:45 +0530

    backend added

--- parent_module/provider.tf
+++ parent_module/provider.tf
@@ -6,14 +6,14 @@ terraform {
     }
   }
 
-#   backend "azurerm" {
-#     use_cli              = true                                    
-#     use_azuread_auth     = true                                    
-#     tenant_id            = "8511c397-31e5-4758-ac6b-099e3eadbe24"  
-#     storage_account_name = "stg5041"                              
-#     container_name       = "tfstate" 
-#     key                  = "task.terraform.tfstate"                              
-# }
+  backend "azurerm" {
+    use_cli              = true                                    
+    use_azuread_auth     = true                                    
+    tenant_id            = "8511c397-31e5-4758-ac6b-099e3eadbe24"  
+    storage_account_name = "stg50411"                              
+    container_name       = "tfstate" 
+    key                  = "task.terraform.tfstate"                              
+}
 }
 
 provider "azurerm" {
