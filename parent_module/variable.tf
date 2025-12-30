55eabf4ddeec3d1752ae426ea0b71d9ed9df8c8b
Shekhar281 <sg.shekhargupta28@gmail.com>
2025-12-30 22:23:43 +0530

    variableinput removed

--- parent_module/variable.tf
+++ parent_module/variable.tf
@@ -6,26 +6,7 @@ variable "vms" {}
 variable "public_ips" {}
 variable "nsgs" {}
 variable "nics" {}
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
 
-variable "client_id" {
-  description = "Azure Service Principal Client ID"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Azure Service Principal Client Secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
 
 # variable "lbs" {}
 # variable "kv" {}
